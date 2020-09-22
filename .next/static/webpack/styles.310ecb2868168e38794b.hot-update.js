27b74fbbae10f215e63a5d75736ba7ea57196877
Waleed-Shahzad <waleedshahzad35@gmail.com>
2020-09-22 14:01:50 +0500

    .gitignore is now working

---
+++ .next/static/webpack/styles.310ecb2868168e38794b.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate("styles",{
+
+/***/ "./node_modules/react-accessible-accordion/dist/fancy-example.css":
+false,
+
+/***/ 4:
+false
+
+})
+//# sourceMappingURL=styles.310ecb2868168e38794b.hot-update.js.map
